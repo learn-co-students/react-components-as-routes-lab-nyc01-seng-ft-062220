2725f428aa8d27fba7726ec44fc1bf68a1221178
yulia <juliana.ny2008@gmail.com>
2020-09-01 17:44:30 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { actors } from '../data';
 
-const Actors = () => {
+class Actors extends React.Component {
+  render(){
+
+    let act = actors.map((obj) => <div>{obj.name}
+                                      <ul>{obj.movies.map(element => <li>{element}</li>
+                                      )}                                         
+                                      </ul>
+                                     </div>
+)
   return (
     <div>
-      {/*{code here}*/}
+      {<h1>Actors Page</h1>}
+      {act}
     </div>
   );
 };
+}
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      {<NavLink  to="/">Home</NavLink>}<br></br>
+      {<NavLink to="/actors">Actors</NavLink>}<br></br>
+      {<NavLink  to="/directors">Directors</NavLink>}<br></br>
+      {<NavLink to="/movies">Movies</NavLink>}
     </div>
   );
 };

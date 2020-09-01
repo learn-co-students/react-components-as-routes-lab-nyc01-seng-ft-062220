268368995b7c5c49bc75eaa1c8ea35ea5b5ab73c
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-09-01 19:27:19 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import { directors } from '../data';
 
-const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-}
+/*
+Again, my code didn't work and i'm not sure why. the only difference that we're creating larger blocsk to insert than usual but I'm not sure why that would
+not work.
+
+Same as Movies - eventually just copied solution
+
+let directorsFormatting = directors.map(
+      directorObj => {
+        return
+        <div>
+          <h3>Name: {directorObj.name}</h3>
+          <p>Movies:</p>
+              <ul>
+                  {directorObj.movies.map(movie => <li>{movie}</li> )}
+              </ul>
+        </div>
+
+      }
+
+*/
+
+
+  const Directors = () => {
+    return (
+      <div>
+        <h1>Directors Page</h1>
+        {directors.map((director, index) => (
+            <div key={index}>
+                <h3>Name: {director.name}</h3>
+                <p>Movies:</p>
+                <ul>
+                    {director.movies.map((movie, index) => (
+                        <li key={index}>{movie}</li>
+                    ))}
+                </ul>
+            </div>
+        ))}
+      </div>
+    );
+  }
 
 export default Directors

aa28bd4bbd0c4b34a3c4ce0ab3b722d862e911d6
hahm-d <dhahm01@gmail.com>
2020-09-02 15:01:27 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'gray',
+    textDecoration: 'none',
+    color: 'white',
+  }
+
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+      <div className="navbar">
+        <NavLink to="/" exact style={link} activeStyle={{background: 'darkgray'}}>Home</NavLink>
+        <NavLink to="/movies" exact style={link} activeStyle={{background: 'darkgray'}}>Movies</NavLink>
+        <NavLink to="/directors" exact style={link} activeStyle={{background: 'darkgray'}}>Directors</NavLink>
+        <NavLink to="/actors" exact style={link} activeStyle={{background: 'darkgray'}}>Actors</NavLink>
+      </div>
+  )
 };
 
 export default NavBar;
